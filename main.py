9618c2627be95fc29da76cf0ad543b829b316c2f
SamiOspreay <150924952+SamiOspreay@users.noreply.github.com>
2025-01-25 13:33:18 +0000

    Add Patient class

--- main.py
+++ main.py
@@ -16,3 +16,19 @@ class EventManager:
         if event.event_type in self.listeners:
             for listener in self.listeners[event.event_type]:
                 listener(event)
+
+
+class Patient:
+    def __init__(self, name, event_manager):
+        self.name = name
+        self.event_manager = event_manager
+
+    def check_in(self):
+        print(f"{self.name} is checking into the hospital.")
+        event = Event("patient_check_in", {"name": self.name})
+        self.event_manager.dispatch(event)
+
+    def request_medication(self, medication):
+        print(f"{self.name} is requesting medication: {medication}.")
+        event = Event("medication_request", {"name": self.name, "medication": medication})
+        self.event_manager.dispatch(event)
