e081a8e13945b18b69619ff66c63d6e27f00ecde
SamiOspreay <150924952+SamiOspreay@users.noreply.github.com>
2025-01-25 13:30:59 +0000

    Add Event and manager

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+class Event:
+    def __init__(self, event_type, data):
+        self.event_type = event_type
+        self.data = data
 
+class EventManager:
+    def __init__(self):
+        self.listeners = {}
+
+    def register_listener(self, event_type, listener):
+        if event_type not in self.listeners:
+            self.listeners[event_type] = []
+        self.listeners[event_type].append(listener)
+
+    def dispatch(self, event):
+        if event.event_type in self.listeners:
+            for listener in self.listeners[event.event_type]:
+                listener(event)
